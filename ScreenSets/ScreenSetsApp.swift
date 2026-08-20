7c4cf7faf7867f42ebbbf573dcd228922b3e8c1c
Sean <itissean@icloud.com>
2026-08-20 13:16:20 +0800

    Initial Commit

---
+++ ScreenSets/ScreenSetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenSetsApp.swift
+//  ScreenSets
+//
+//  Created by Sean on 2026/8/20.
+//
+
+import SwiftUI
+
+@main
+struct ScreenSetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
